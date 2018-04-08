e979e72d6e70a3afa6558bc09dff38a58033f3ce
Eleanor Demis <eleanordemis@gmail.com>
2018-04-08 14:17:05 -0700

    sets up express with babel

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './routes';
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use('/', router);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("http://localhost:" + port)
+});
